d183abe476fe3517c0c40cc0c791a121f10fae34
Luke Taylor <Luke@circleleasing.co.uk>
2022-12-21 13:18:46 +0000

    Day two part two

--- 2/strategy.js
+++ 2/strategy.js
@@ -29,6 +29,23 @@ const moveScores = {
 let score = 0;
 readInput().forEach(line => {
 	const moves = line.split(" ");
+
+	const choices = outcomes[moves[0]];
+	let targetScore;
+	if(moves[1] === 'X'){
+		targetScore = 0;
+	} else if(moves[1] === 'Y'){
+		targetScore = 3;
+	} else {
+		targetScore = 6;
+	}
+
+	Object.keys(choices).forEach(k => {
+		if(choices[k] === targetScore){
+			moves[1] = k;
+		}
+	});
+
 	const outcome = outcomes[moves[0]][moves[1]];
 	const moveScore = moveScores[moves[1]];
 	score += outcome + moveScore;
