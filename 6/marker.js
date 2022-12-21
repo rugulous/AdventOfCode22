76aeba9f0ad2471247b60f9c5459d6ce45305e98
Luke Taylor <Luke@circleleasing.co.uk>
2022-12-21 16:06:22 +0000

    6

---
+++ 6/marker.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const {readInput} = require('../helper');
+
+const input = readInput()[0];
+
+let index = -1;
+
+for(let i = 0; i < input.length; i++){
+	const chunk = input.slice(i, i + 4);
+	
+	let found = false;
+	for(let j = 0; j < chunk.length; j++){
+		if(chunk.indexOf(chunk[j]) != chunk.lastIndexOf(chunk[j])){
+			found = true;
+			break;
+		} else {
+			index = i + j;
+		}
+	}
+
+	if(!found){
+		console.log(index + 1);
+		console.log(chunk);
+		break;
+	}
+}
